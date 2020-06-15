3a380fe75aa667b5ea040e1fe9f98aba014f89b0
Mukesh Murali <mukesh.murali@gamesys.co.uk>
2020-06-16 00:03:13 +0100

    Fixing virtuanenvcreate

--- virtualenv-create.sh
+++ virtualenv-create.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
-ENV_FOLDER=~/virtualenv/
-PROJECT=minikube-configuration5
-PROJECT_ENV_FOLDER=$ENV_FOLDER/$PROJECT
-
-rm -rf $PROJECT_ENV_FOLDER
-virtualenv $PROJECT_ENV_FOLDER --no-pip
-
-. $PROJECT_ENV_FOLDER/bin/activate
+#!/usr/bin/env bash
+rm -rf ~/virtualenv/minikube-configuration
+virtualenv ~/virtualenv/minikube-configuration --no-pip
+. ~/virtualenv/minikube-configuration/bin/activate
 pip3 install -r requirements_minikube.txt
 
